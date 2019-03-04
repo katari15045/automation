e384c952c26bdb62e85c3f596fcb45c6e1693b71
Saketh Katari <katari15045@iiitd.ac.in>
2019-03-04 15:45:33 +0530

    2_youtube_playlist: able to download mp3s of all songs in a playlist

--- 2_youtube_playlist/main.py
+++ 2_youtube_playlist/main.py
@@ -1,4 +1,9 @@
 from requests import get
+from youtube_dl import YoutubeDL
+
+''' Task: Given a youtube playlist, download mp3 version of each song; filename of a downloaded .mp3 version of a song - 
+should start with "song's position number in playlist" 
+'''
 
 class Main:
 
@@ -10,22 +15,26 @@ class Main:
 	@staticmethod
 	def main():
 		url_api = "https://www.googleapis.com/youtube/v3/playlistItems"
-		params_api = {"part":"snippet", "maxResults":Main.max_results, "playlistId":Main.playlist_id, 'key':Main.api_key}		
-		url_videos = []
+		params_g_api = {"part":"snippet", "maxResults":Main.max_results, "playlistId":Main.playlist_id, 'key':Main.api_key}		
 		next_page_token = "dummy"
+		song_num = 1
 		while(next_page_token != None):
-			response = get(url=url_api, params=params_api)
+			response = get(url=url_api, params=params_g_api)
 			response = response.json()
 			ind = 0
 			while(ind < len(response["items"])):
 				video_id = response["items"][ind]["snippet"]["resourceId"]["videoId"]
+				video_title = response["items"][ind]["snippet"]["title"]
+				video_title = video_title.replace(" ", "_")
+				video_title = str(song_num) + "_" + video_title
 				url_video = "https://www.youtube.com/watch?v=" + video_id
-				url_videos.append(url_video)
+				params_y_api = {'format':'bestaudio', 'outtmpl': video_title + '.%(ext)s'}
+				with YoutubeDL(params_y_api) as ydl:
+					print("Song " + str(song_num) + " ...")
+					ydl.download([url_video])
+				song_num = song_num+1
 				ind = ind+1
 			next_page_token = response.get("nextPageToken")
 			if(next_page_token != None):
-				params_api["pageToken"] = next_page_token
-		print(url_videos)
-		print(len(url_videos))
-
+				params_g_api["pageToken"] = next_page_token
 Main.main()
